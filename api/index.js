5bb3e763abfaeb32860e76a36209225781765ddb
unknown <shrikanthp01@gmail.com>
2023-05-10 14:59:52 +0530

    initial-commit

---
+++ api/index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://shrikanth:emQ01ApYdVk3UDx1@cluster0.kthxogs.mongodb.net/booking?retryWrites=true&w=majority"
+    );
+    console.log("Connected to mongodb.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected");
+});
+
+app.use(cookieParser);
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errStatus = err.status || 500;
+  const errMessage = err.message || "Something went wrong";
+  return res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen("8800", () => {
+  connect();
+  console.log("Connected to backend");
+});
